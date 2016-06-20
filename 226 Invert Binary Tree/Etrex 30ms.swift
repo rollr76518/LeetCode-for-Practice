11849dd72431933e70d2350fdd4091f1f4c5b044
etrex kuo <et284vu065k3@gmail.com>
2016-06-20 23:02:39 +0800

    226

---
+++ 226 Invert Binary Tree/Etrex 30ms.swift
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(root: TreeNode?) -> TreeNode? {
+        guard let root = root else {return nil}
+        (root.left,root.right) = (invertTree(root.right), invertTree(root.left))
+        return root
+    }
+}
