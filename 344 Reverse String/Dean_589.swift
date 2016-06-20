befa5cefb806d092f18f95d301d6e434439db48b
dean chen <chunchangchen@gmail.com>
2016-06-20 22:42:51 +0800

    update dean's solution for 008, 217, 258, and 344

---
+++ 344 Reverse String/Dean_589.swift
@@ -0,0 +1,16 @@
+//
+//  Dean_589.swift
+//  MyLeetCode
+//
+//  Created by dean on 2016/6/20.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+import Foundation
+
+class Solution {
+    func reverseString(s: String) -> String {
+        let rs = s.characters.reverse()
+        return String(rs)
+    }
+}
